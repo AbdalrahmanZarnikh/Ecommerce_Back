c948bf94b63368895733efc34485c289be3b5f09
AbdAlrahman13 <blalabo89@gmail.com>
2025-07-04 15:22:35 +0300

    finish order controllers and routes

--- utils/validators/userValidator.js
+++ utils/validators/userValidator.js
@@ -2,8 +2,8 @@ const { check } = require("express-validator");
 
 const validatorMiddleware = require("../../middlewares/ValidatorMiddleware");
 
-const UserModel=require("../../models/userModel")
- 
+const UserModel = require("../../models/userModel");
+
 const { default: slugify } = require("slugify");
 
 const getUserValidator = [
@@ -26,66 +26,67 @@ const createUserValidator = [
     .withMessage("please enter a valid email address")
     .isEmail()
     .withMessage("Invalid email address")
-    .custom((value)=>
-       UserModel.findOne({email:value}).then((user)=>{
-        if(user){
-          return Promise.reject(new Error("email already exists"))
+    .custom((value) =>
+      UserModel.findOne({ email: value }).then((user) => {
+        if (user) {
+          return Promise.reject(new Error("email already exists"));
         }
-       })
-    )
-    ,
+      })
+    ),
+  check("password")
+    .notEmpty()
+    .withMessage("password required")
+    .isLength({ min: 6 })
+    .withMessage("password must be at least 6 characters"),
 
-    check("password").notEmpty().withMessage("password required").isLength({min:6}).withMessage("password must be at least 6 characters"),
-
-    validatorMiddleware,
+  validatorMiddleware,
 ];
 
 const updateUserFromAdmin = [
-   check("name")
+  check("name")
     .optional()
     .custom((val, { req }) => {
-    req.body.slug = slugify(val);
-    return true;
-  }),
+      req.body.slug = slugify(val);
+      return true;
+    }),
   check("email")
-    .notEmpty()
-    .withMessage("please enter a valid email address")
+    .optional()
     .isEmail()
     .withMessage("Invalid email address")
-    .custom((value)=>
-       UserModel.findOne({email:value}).then((user)=>{
-        if(user){
-          return Promise.reject(new Error("email already exists"))
+    .custom((value) =>
+      UserModel.findOne({ email: value }).then((user) => {
+        if (user) {
+          return Promise.reject(new Error("email already exists"));
         }
-       })
-    )
-    ,
+      })
+    ),
+  check("role").optional(),
 
-    check("role").optional(),
-
-  validatorMiddleware];
+  validatorMiddleware,
+];
 
 const updateUserProfileValidator = [
-   check("name")
+  check("name")
     .optional()
     .custom((val, { req }) => {
-    req.body.slug = slugify(val);
-    return true;
-  }),
+      req.body.slug = slugify(val);
+      return true;
+    }),
   check("email")
     .notEmpty()
     .withMessage("please enter a valid email address")
     .isEmail()
     .withMessage("Invalid email address")
-    .custom((value)=>
-       UserModel.findOne({email:value}).then((user)=>{
-        if(user){
-          return Promise.reject(new Error("email already exists"))
+    .custom((value) =>
+      UserModel.findOne({ email: value }).then((user) => {
+        if (user) {
+          return Promise.reject(new Error("email already exists"));
         }
-       })
+      })
     ),
-  
-  validatorMiddleware];
+
+  validatorMiddleware,
+];
 
 const deleteUserValidator = [
   check("id").isMongoId().withMessage("invalid user id"),
