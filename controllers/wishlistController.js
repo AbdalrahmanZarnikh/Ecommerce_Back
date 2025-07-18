3b9a7ed795e9423e12f0ba75baa0d2455565d144
AbdAlrahman13 <blalabo89@gmail.com>
2025-07-18 15:30:22 +0300

    finish order controllers and routes

--- controllers/wishlistController.js
+++ controllers/wishlistController.js
@@ -6,41 +6,12 @@ const ProductModel = require("../models/productModel");
 exports.AddProductToWishlist = asyncHandler(async (req, res, next) => {
   const product = await ProductModel.findById(req.body.productId);
   if (!product) {
-    return next(new Error("product not found"));
+    return next(new Error("Product not found"));
   }
 
   const user = await UserModel.findOneAndUpdate(
-    req.user._id,
-    {
-      $addToSet: { wishlist: req.body.productId },
-    },
-    { new: true }
-  );
-
-  const wishlistWithProducts = await Promise.all(
-    user.wishlist.map(async (id) => {
-      const product = await ProductModel.findById(id);
-      return product ? id : null;
-    })
-  );
-
-  user.wishlist = wishlistWithProducts.filter((id) => id !== null);
-
-  await user.save();
-
-  res.status(200).json({
-    status: "Success",
-    message: "Product Added Successfully To Wishlist ",
-    data: user.wishlist,
-  });
-});
-
-exports.RemoveProductFromWishlist = asyncHandler(async (req, res, next) => {
-  const user = await UserModel.findOneAndUpdate(
     { _id: req.user._id },
-    {
-      $pull: { wishlist: req.params.productId },
-    },
+    { $addToSet: { wishlist: req.body.productId } },
     { new: true }
   );
 
@@ -48,22 +19,55 @@ exports.RemoveProductFromWishlist = asyncHandler(async (req, res, next) => {
     return next(new Error("User not found"));
   }
 
-  const wishlistWithProducts = await Promise.all(
+  const validProductIds = await Promise.all(
     user.wishlist.map(async (id) => {
       const product = await ProductModel.findById(id);
       return product ? id : null;
     })
   );
 
-  user.wishlist = wishlistWithProducts.filter((id) => id !== null);
+  user.wishlist = validProductIds.filter((id) => id !== null);
   await user.save();
+
+  const populatedUser = await UserModel.findById(user._id).populate("wishlist");
+
+  res.status(200).json({
+    status: "Success",
+    message: "Product added successfully to wishlist",
+    data: populatedUser.wishlist
+  });
+});
+
+exports.RemoveProductFromWishlist = asyncHandler(async (req, res, next) => {
+  const user = await UserModel.findOneAndUpdate(
+    { _id: req.user._id },
+    { $pull: { wishlist: req.params.productId } },
+    { new: true }
+  );
+
+  if (!user) {
+    return next(new Error("User not found"));
+  }
+
+  const validProductIds = await Promise.all(
+    user.wishlist.map(async (id) => {
+      const product = await ProductModel.findById(id);
+      return product ? id : null;
+    })
+  );
+
+  user.wishlist = validProductIds.filter((id) => id !== null);
+  await user.save();
+
+  const populatedUser = await UserModel.findById(user._id).populate("wishlist");
 
   res.status(200).json({
     status: "success",
     message: "Product removed successfully from wishlist",
-    data: user.wishlist,
+    data: populatedUser.wishlist
   });
 });
+
 
 exports.GetLoggedUserWishlist = asyncHandler(async (req, res, next) => {
   const user = await UserModel.findById(req.user._id).populate("wishlist");
